fc46a971f6de52eea31323813e155aaa4f99daaa
vnylbscr <mertgenc8@gmail.com>
2021-06-17 22:33:53 +0300

    changed with hooks

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,35 +1,41 @@
-import React, { useRef, useState, useLayoutEffect } from 'react'
-import { connect } from "react-redux";
-import { addTodoList } from "../actions/index";
+import React, { useRef, useState, useLayoutEffect } from "react";
 import TodoList from "./TodoList";
+import { useDispatch } from "react-redux";
+import { addTodoList } from "../actions";
+const Todo = () => {
+  const inputRef = useRef(null);
+  const [input, setInput] = useState();
+  const dispatch = useDispatch();
 
-const Todo = (props) => {
-    const inputRef = useRef(null);
-    const [input, setInput] = useState();
-    useLayoutEffect(() => {
-        inputRef.current.focus();
-    }, []);
-    return (
-        <div className="container">
-            <h1 className="header-title">
-                Todos App with Redux
-            </h1>
-            <div className="input-area">
-                <input onChange={(e) => setInput(e.target.value)} className="input-element" value={input || ""} type="text" ref={inputRef} placeholder="Add new task"/>
-                {
-                    input &&
-                    <button className="btn-add"
-                        onClick={() => {
-                            props.addTodoList(input)
-                            setInput("");
-                        }}
-                        >
-                        Add new task
-                </button>
-                }
-            </div>
-            <TodoList />
-        </div>
-    )
-}
-export default connect(null, { addTodoList })(Todo);
+  useLayoutEffect(() => {
+    inputRef.current.focus();
+  }, []);
+  return (
+    <div className="container">
+      <h1 className="header-title">+ Todos App with Redux</h1>
+      <div className="input-area">
+        <input
+          onChange={(e) => setInput(e.target.value)}
+          className="input-element"
+          value={input || ""}
+          type="text"
+          ref={inputRef}
+          placeholder="Add new task"
+        />
+        {input && (
+          <button
+            className="btn-add"
+            onClick={() => {
+              dispatch(addTodoList(input));
+              setInput("");
+            }}
+          >
+            Add new task
+          </button>
+        )}
+      </div>
+      <TodoList />
+    </div>
+  );
+};
+export default Todo;
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,32 +1,32 @@
-import React from 'react'
-import { connect } from 'react-redux';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { completedToItemFromToDoList, removeTodoList } from "../actions/index";
+const TodoList = () => {
+  const list = useSelector((state) => state.todoList);
+  const dispatch = useDispatch();
+  console.log(dispatch);
+  return (
+    list &&
+    list.length > 0 &&
+    list.map((item, index) => {
+      return (
+        <div className="list-container" key={index}>
+          <div
+            className={item.completed ? "not-active" : "active"}
+            onClick={() => dispatch(completedToItemFromToDoList(item.id))}
+          >
+            {item.text}
+          </div>
+          <i
+            onClick={() => dispatch(removeTodoList(item.id))}
+            className="fas fa-window-close icon remove-container"
+          >
+            <p className="tooltip">Remove task</p>
+          </i>
+        </div>
+      );
+    })
+  );
+};
 
-const TodoList = (props) => {
-    const { list } = props;
-    return (
-        list && list.length > 0 && list.map((item,index) => {
-            return (
-                <div className="list-container" key={index}>
-                    <div className={item.completed ? "not-active" : "active"}
-                        onClick={() => props.completedToItemFromToDoList(item.id)}
-                    >
-                        {item.text}
-                    </div>
-                    <i onClick={() => props.removeTodoList(item.id)} 
-                    className="fas fa-window-close icon remove-container">
-                        <p className="tooltip">
-                            Remove task
-                        </p>
-                    </i>
-                </div>
-            )
-        })
-    )
-}
-const mapStateToProps = (state) => {
-    return {
-        list: state.todoList
-    }
-}
-export default connect(mapStateToProps, { completedToItemFromToDoList, removeTodoList })(TodoList);
+export default TodoList;
