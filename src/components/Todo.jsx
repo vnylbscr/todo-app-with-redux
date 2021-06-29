4e2b358028c796a4606efc43f1261d1b5b300994
vnylbscr <mertgenc8@gmail.com>
2021-06-29 22:19:21 +0300

    testtt

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,29 +1,26 @@
 import React, { useRef, useState, useLayoutEffect } from "react";
 import TodoList from "./TodoList";
-import {useDispatch}  from "react-redux";
+import { useDispatch } from "react-redux";
 import { addTodoList } from "../actions";
-import Merhaba from "./Merhaba"
+import Merhaba from "./Merhaba";
+import useSize from "../helper/useSize";
 const Todo = () => {
   const inputRef = useRef(null);
   const [input, setInput] = useState();
-  const [state, setState] = useState({
-    mail:'',
-    password:'',
-    confitPass:null
-  })
 
-
-  
-  
   const dispatch = useDispatch();
   useLayoutEffect(() => {
     inputRef.current.focus();
+    
   }, []);
-  
+  const { width, height } = useSize();
+  const [state, setState] = useState("");
+
+  console.log(state);
   return (
-    <div className='container'>
+    <div className="container">
       <h1 className="header-title">+ Todos App with Redux</h1>
-      <div className="input-area" >
+      <div className="input-area">
         <input
           onChange={(e) => setInput(e.target.value)}
           className="input-element"
@@ -33,6 +30,7 @@ const Todo = () => {
           placeholder="Add new task"
           name="input"
           
+          name="input"
         />
         {input && (
           <button
@@ -46,13 +44,23 @@ const Todo = () => {
           </button>
         )}
       </div>
-      <Merhaba name="Ömer" value="mert">
-      </Merhaba>
+      <button
+        onClick={() => {
+          setTimeout(() => {
+            setState("falan filan");
+          }, 5000);
+        }}
+      >
+        çalıştır
+      </button>
+      {state && <p>{state}</p>}
+      <span>
+        W:{width}
+        H:{height}
+      </span>
+      <Merhaba name="Ömer" value="mert"></Merhaba>
       <TodoList />
     </div>
   );
 };
 export default Todo;
-
-const width = useTime();
-
